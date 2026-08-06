6fec26c499cd8f89510dca346c9e90a10e9b4696
Amogh Parmar <amoghparmar30@gmail.com>
2026-08-06 18:40:33 +0530

    fix: standardize Create Hold Task dialog layout and task name behavior (#455)

--- src/views/OrderDetail.vue
+++ src/views/OrderDetail.vue
@@ -3297,8 +3297,6 @@ async function openCreateHoldTaskModal() {
     componentProps: {
       shipGroups,
       title: translate('Create hold task'),
-      autoGenerateTaskName: true,
-      defaultOrderName: currentOrder.orderName || currentOrder.id,
       defaultWorkEffortPurposeTypeId: 'ORD_HOLD_MANUAL',
     },
   });
