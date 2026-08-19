612e3444412d660cfb57196f288d0697702ea3a7
Yash Maheshwari <yashmaheshwari551@gmail.com>
2026-08-19 19:55:22 +0530

    Fixed: issue when rejecting multiple items or moving multiple items to parking

--- src/views/OrderDetail.vue
+++ src/views/OrderDetail.vue
@@ -3684,15 +3684,13 @@ async function parkSelectedItems(shipGroup: any) {
   if (!facilityId) return;
   const orderId = order.value!.id;
   try {
-    await Promise.all(
-      itemIds.map((orderItemSeqId) =>
-        api({
+    for(let orderItemSeqId of itemIds) {
+      await api({
         url: `oms/orders/${orderId}/moveItemToParking`,
         method: 'POST',
         data: { orderId, orderItemSeqId, shipGroupSeqId: shipGroup.id, toFacilityId: facilityId },
-        })
-      )
-    );
+      })
+    }
     selectedShipGroupItems.value[shipGroup.id] = new Set();
     await showToast(translate('Items moved to parking.'));
     await loadOrder(orderId, true);
@@ -3756,19 +3754,18 @@ async function releaseSelectedItems(shipGroup: any) {
   if (!facilityId) return;
   const orderId = order.value!.id;
   try {
-    await Promise.all(
-      itemIds.map((orderItemSeqId) =>
-        api({
+    for(let orderItemSeqId of itemIds) {
+      await api({
         url: `oms/orders/${orderId}/items/${orderItemSeqId}/allocation`,
         method: 'POST',
-        data: { facilityId,
-         orderFacilityChange:{
-         changeReasonEnumId: "RELEASED"
-         }
-         },
-        })
-      )
-    );
+        data: {
+          facilityId,
+          orderFacilityChange:{
+          changeReasonEnumId: "RELEASED"
+          }
+        },
+      })
+    }
     selectedShipGroupItems.value[shipGroup.id] = new Set();
     await showToast(translate('Items released to facility.'));
     await loadOrder(orderId, true);
